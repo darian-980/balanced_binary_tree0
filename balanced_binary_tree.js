3ec0c9c9509267aeb4afed28388526ed177b8a02
Devolas <darian.dihal@gmail.com>
2026-04-21 14:54:18 -0400

    deleteItem works (both children), however it needs to be redesigned to replace the base value instead of pointers

--- balanced_binary_tree.js
+++ balanced_binary_tree.js
@@ -97,10 +97,8 @@ function binaryTreeMake(passedArray) {
     function deleteItem(deleteValue) {
         function deleteInner(deleteValue, base = tree, previousNode = null) {
             if (deleteValue === base.value) {
-                const newNode = createNode();
-                newNode.value = deleteValue;
 
-                if (base.leftNode === null && base.rightNode === null) { //target has no children // both NULL
+                if (base.leftNode === null && base.rightNode === null) { //target has no children (both NULL)
                     if (previousNode.leftNode === base) {
                         previousNode.leftNode = null;
                     }
@@ -121,9 +119,10 @@ function binaryTreeMake(passedArray) {
                         previousNode.rightNode = validNode;
                     }
                 }
-                
-                else if (base.leftNode !== null || base.rightNode !== null){
-                    
+
+                else if (base.leftNode !== null || base.rightNode !== null) { //target has 2 children (both non-null)
+                    console.log("swap removal")
+                    swapRemoval(base, base, previousNode, previousNode, "left")
                 }
             }
 
@@ -135,6 +134,67 @@ function binaryTreeMake(passedArray) {
                 return deleteInner(deleteValue, base.leftNode, base)
             }
         }
+
+        function swapRemoval(base, swapNode, reattachNode, previousNode, direction, rightOnce = true) { //function used to remove a node that has 2 children (left and right)
+            if (direction === "left") {
+                if (base.leftNode === null || base.leftNode === undefined) {
+
+                    console.log(base.value)
+                    if (swapNode === tree) { console.log("base.leftnode is " + base.leftnode + " swapNode is " + swapNode.value + " direction is " + direction) }
+                    else { console.log("base.leftnode is " + base.leftnode + " swapNode is " + swapNode.value + " reattachNode is " + reattachNode.value + " direction is " + direction) }
+
+                    if (base.rightNode !== null) {
+                        previousNode.leftNode = base.rightNode; // if the innermost leftnode we are going to swap has a child right node, we need to assign it to the previous node's left child so that the data doesn't get lost
+                    } else {
+                        previousNode.leftNode = null;
+                    }
+                    if (swapNode !== tree) {
+                        if (reattachNode.leftNode === swapNode) { //if the stem of the node we are replacing has it on the left side, assign it to the left side
+                            reattachNode.leftNode = base;
+                        }
+                        else {
+                            reattachNode.rightNode = base;
+                        }
+                    }
+
+                    base.leftNode = swapNode.leftNode; //overrite the innermost left node's left path with the original node's LEFT path (taking it's place)
+                    base.rightNode = swapNode.rightNode; //overrite the innermost left node's left path with the original node's RIGHT path (taking it's place)
+                    if (swapNode === tree) {
+                        console.log("tet")
+                        tree = base;
+                        console.log(tree.value)
+                    }
+                    swapNode = null; //removing the original node completely
+                }
+                else {
+                    console.log(base.value)
+                    if (rightOnce === true) {
+                        swapRemoval(base.rightNode, swapNode, reattachNode, base, direction, false) // go right once and then after that only go left to get the closest greater value
+                    }
+                    else {
+                        swapRemoval(base.leftNode, swapNode, reattachNode, base, direction, false)
+                    }
+
+                }
+            }
+            else if (direction === "right") {
+                if (base.rightNode === null) {
+                    if (base.leftNode !== null) {
+                        previousNode.rightNode = base.leftNode; // if the innermost rightnode we are going to swap has a child right node, we need to assign it to the previous node's left child so that the data doesn't get lost
+                    }
+                    base.leftNode = swapNode.leftNode; //overrite the innermost right node's left path with the original node's LEFT path (taking it's place)
+                    base.rightNode = swapNode.rightNode; //overrite the innermost right node's left path with the original node's RIGHT path (taking it's place)
+                    swapNode = null; //removing the original node completely
+                }
+                else {
+                    swapRemoval(base.rightNode, swapNode, reattachNode, base, direction)
+                }
+            }
+
+            return false;
+
+        }
+
 
         return deleteInner(deleteValue);
     }
@@ -182,8 +242,16 @@ console.log(newTree.includes(7645))
 console.log(newTree.insert(6))
 console.log(newTree.insert(8940))
 console.log(newTree.insert(332))
-newTree.prettyPrint(newTree.tree)
+// newTree.prettyPrint(newTree.tree)
 
 
-console.log(newTree.deleteItem(7645));
+// console.log(newTree.deleteItem(7645));
+// newTree.prettyPrint(newTree.tree)
+
 newTree.prettyPrint(newTree.tree)
+console.log(newTree.deleteItem(765));
+newTree.prettyPrint(newTree.tree)
+console.log(newTree.deleteItem(34));
+newTree.prettyPrint(newTree.tree)
+console.log(newTree.tree.value);
+//
